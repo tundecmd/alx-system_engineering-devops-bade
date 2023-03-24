681c9a01c2774171e6292e176f1905d10881a44a
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-24 17:47:50 +0100

    attempted task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,15 @@
+# This manifest creates a file at /tmp/school
+
+$filepath = '/tmp/school'
+$filemode = '0744'
+$fileowner = 'www-data'
+$filegroup = 'www-data'
+$filecontent = "I love Puppet\n"
+
+file { $filepath:
+  ensure  => 'file',
+  mode    => $filemode,
+  owner   => $fileowner,
+  group   => $filegroup,
+  content => $filecontent,
+}
