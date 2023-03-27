585317a2ef032c593da0a160309cae4a166279cb
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-27 04:48:38 +0100

    attempted task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# This puppet script creates an ssh config file
+
+file_line { 'Turn off passwd auth':
+	ensure => 'present',
+	path => '/etc/ssh/ssh_config',
+	line => '	PasswordAuthentication no'
+	replace => true,
+}
+file_line { 'Declare identity file':
+	ensure => 'present',
+	path => '/etc/ssh/ssh_config',
+	line => '	IdentityFile ~/.ssh/school',
+}
